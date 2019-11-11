f2d3697a398b91825c30231ba24980e24c8e3f02
Diego Ribeiro <ribesites@hotmail.com.br>
2019-11-11 18:04:28 -0300

    Subindo site para github

---
+++ php/footer.php
@@ -0,0 +1,69 @@
+<footer class="bg-dark mg">
+    <div class="bg-dark text-white">
+	    <div class="container">
+    		  <div class="row">
+                <div class="p-4 col-md-3">
+                  <h2 class="mb-4 text-secondary">Agência Ribsites</h2>
+                  <p class="text-white">Sites desenvolvidos e pensados no seu cliente. Criamos sites acessíveis a audiovisuais.</p>
+                </div>
+                <div class="p-4 col-md-3">
+                  <h2 class="mb-4 text-secondary">Sitemap</h2>
+                  <ul class="list-unstyled">
+                    <a href="index.php" class="text-white">Inicio</a>
+                    <br>
+                    <a href="sobre.php" class="text-white">Sobre</a>
+                    <br>
+                    <a href="contato.php" class="text-white">Contato</a>
+                    <br>
+                    <a href="duvidas.php" class="text-white">Perguntas frequentes</a> 
+                  </ul>
+                </div>
+                <div class="p-4 col-md-3">
+                  <h2 class="mb-4">Contato</h2>
+                  <p>
+                    <a href="tel:4133923426	" class="text-white"><i class="fa d-inline mr-3 text-secondary fa-phone"></i>(41)92000-8829|WhatsApp</a>
+                  </p>
+                  <p>
+                    <a href="mailto:info@pingendo.com" class="text-white"><i class="fa d-inline mr-3 text-secondary fa-at"></i> ribsites@hotmail.com.br</a>
+                  </p>
+                  <p>
+                    <a href="https://goo.gl/maps/AUq7b9W7yYJ2" class="text-white" target="_blank"><i class="fa d-inline mr-3 fa-map-marker text-secondary"></i>Batista Sabim, 600 Campo Largo/Paraná</a>
+                  </p>
+                </div>
+                <div class="p-4 col-md-3">
+                  <h2 class="mb-4 text-light">Se inscrever</h2>
+                  <form>
+                    <fieldset class="form-group text-white"> <label for="exampleInputEmail1">Receber ultimas novidades.</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email"> </fieldset>
+                    <button type="submit" class="btn btn-outline-secondary">Newsletter</button>
+                  </form>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 mt-3">
+                  <p class="text-center text-white">© Copyright 2019 Agência Ribsites  - All rights reserved. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+  
+  
+  
+   <!--ERROOOOOOOOOOOOOOOOOOOOOOO Optional JavaScript -->
+  <script src="js/jquery.js" type="text/javascript" ></script>
+  
+  
+    
+    
+       
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/bootstrap.js" type="text/javascript"></script>
+    <script src="js/popper.js" type="text/javascript"></script>
+    <script src="js/bootstrap.bundle.js" type="text/javascript"></script>
+    
+    
+    
+ 
+  </body>
+</html>
